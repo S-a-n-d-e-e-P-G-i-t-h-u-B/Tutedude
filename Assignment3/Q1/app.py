e3c0b2a65f67663993228900794e2e85899fb0b0
Sandeep Singh <161930926+S-a-n-d-e-e-P-G-i-t-h-u-B@users.noreply.github.com>
2026-07-24 18:56:21 +0530

    Add Flask API to serve JSON data

--- Assignment3/Q1/app.py
+++ Assignment3/Q1/app.py
@@ -1,1 +1,19 @@
+import json
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/api")
+def api():
+
+    file = open("data.json", "r")
+    
+    data = json.load(file)
+    
+    file.close()
+    
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
